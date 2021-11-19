9e287c45c74e974289aced4d40c92c8b085e5d03
unknown <you@example.com>
2021-11-19 20:06:27 +0530

    Added: Code and Test case to check the vehicle unparked

--- src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.parkinglotsystem;
 
 public class ParkingLotSystem {
+    private Object vehicle;
+
     public ParkingLotSystem() {
     }
 
@@ -11,6 +13,23 @@ public class ParkingLotSystem {
      * @return True For Vehicle Parked
      */
     public boolean park(Object vehicle) {
+        if(this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
         return true;
+    }
+
+    /**
+     * Purpose: To check given vehicle is parked to unpark it
+     * @param vehicle
+     * @return True for vehicle unparked
+     */
+    public boolean unPark(Object vehicle) {
+        if (this.vehicle == null) return false;
+        if(this.vehicle.equals(vehicle)){
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/bridgelabz/parkinglotsystem/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/parkinglotsystem/ParkingLotSystemTest.java
@@ -1,13 +1,40 @@
 package com.bridgelabz.parkinglotsystem;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParkingLotSystemTest {
+    ParkingLotSystem parkingLotSystem = null;
+    Object vehicle = null;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
+
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-       ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
        boolean isParked = parkingLotSystem.park(new Object());
        assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+        parkingLotSystem.park(vehicle);
+        boolean isParked = parkingLotSystem.park(new Object());
+        assertFalse(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLotSystem.park(vehicle);
+        boolean isUnParked = parkingLotSystem.unPark(vehicle);
+        assertTrue(isUnParked);
     }
 }
